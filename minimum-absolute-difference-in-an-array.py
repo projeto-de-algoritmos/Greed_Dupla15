032621ef4ed7cdb015d61ef3d8cf8dce099110ef
Ítalo Vinícius <italovinicius18@yahoo.com>
2022-03-04 08:50:56 -0300

    Questão 1 adicionada

---
+++ minimum-absolute-difference-in-an-array.py
@@ -0,0 +1,32 @@
+# Questão "Minimum Absolute Difference in an Array" do HackerRank
+# https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumAbsoluteDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def minimumAbsoluteDifference(arr):
+    arr.sort() # Ordenando tarefas que nos interessam
+    
+    diferencas = [] # Todas as tarefas nos interessam no array recebido, entao sera uma variavel que vamos utilizar em todas as manipulacoes dos intervalos
+    
+    diferencas.append(abs(arr[0] - arr[1])) # Esta e a diferenca minima para o primeiro elemento do array
+    
+    diferencas.append(abs(arr[n - 1] - arr[n - 2])) # Esta e a diferenca minima para o ultimo elemento do array
+         
+    for i in range(1, n - 1): # Percorre todo o array buscando a minima diferenca absoluta
+        diferencas.append(min(abs(arr[i] - arr[i - 1]),
+                  abs(arr[i] - arr[i + 1])))
+                
+    return min(diferencas) # Retornamos a menor diferenca encontrada na lista de diferencas
